532f379e784a91538b02322c396b3099cc1762f6
StevieReikine <37851532+StevieReikine@users.noreply.github.com>
2018-04-02 17:40:48 +0100

    Add files via upload
    two human players can play

--- pentago_main.py
+++ pentago_main.py
@@ -7,7 +7,7 @@ class Player:
         self.id = id                #the id also used to mark pieces on the board
      # perform a play action   
     def play(self,  board):
-        print("player " + self.name + " is playing.")
+        #print("player " + self.name + " is playing.")  #stupid nonsense Jarek put in
         return 0
         
         
@@ -16,8 +16,8 @@ class HumanPlayer(Player):
         validMove = False;
         while not validMove:
             x, y = [int(x) for x in raw_input("Enter the coordinate to play (x, y): ").split(',')]
-            #print x # x-coordinate to human user (index at 1)
-            #print y #y-coordinate to human user (index at 1)
+            # x-coordinate to human user (index at 1)
+            # y-coordinate to human user (index at 1)
             if a_board.Get(y-1,x-1) == 0: 
                 a_board.AddPiece(y-1, x-1,  self.id)
                 validMove = True;
@@ -31,10 +31,12 @@ class HumanPlayer(Player):
 
 class Board:
     def __init__(self):
-        self.boardmtx = np.zeros((6,6))
-        self.boardmtx[1,1]=1
-        self.boardmtx[1,2]=2
-        self.boardmtx[0,5]=1
+        self.boardmtx = np.zeros((6,6),dtype=np.int)
+        #self.boardmtx[0,0] = 1
+        #self.boardmtx[0,1] = 1
+        #self.boardmtx[0,2] = 1
+        #self.boardmtx[0,3] = 1
+        
         print(self.boardmtx)
         
     def AddPiece(self, x, y, value):
@@ -77,7 +79,7 @@ class Board:
       rotation = raw_input("Which direction to rotate (C or CC)?")
       print(rotation)
       #print rotation
-      direction = -1
+      direction = 0
       if rotation == "C":
           direction = 3
       elif rotation == "CC":
@@ -94,10 +96,52 @@ class Board:
       print(self.boardmtx)
       return 0
 
+    def GameEnd(self):
+        diag1 = np.diag(self.boardmtx, k=-1).copy().tolist()+[0]
+        diag2 = np.diag(self.boardmtx, k=1).copy().tolist()+[0]
+        diag3 = np.diag(self.boardmtx).copy().tolist()
+        diag4 = np.diag(np.rot90(self.boardmtx)).copy().tolist()   #off-diagonal, middle
+        diag5 = np.diag(np.rot90(self.boardmtx), k=1).copy().tolist()+[0]
+        diag6 = np.diag(np.rot90(self.boardmtx), k=-1).copy().tolist()+[0]
+        diagonals = np.array([diag1, diag2, diag3, diag4, diag5, diag6])
+        columns = np.rot90(self.boardmtx)
+        IsGameOver = False
+        def boardcheck(matrix):
+            for row in matrix:
+                if row[1] != 0:
+                    print(row[0], row[1], row[2], row[3], row[4],row[5])
+                    if (row[1] == row[2]) and (row[1] == row[3]) and (row[1] == row[4]) and (row[1] == row[5] or row[0] == row[1]):
+                        print("Game over."), 
+                        if row[1] == 1:
+                            print("Player 1 wins.")
+                        elif row[1] == 2:
+                            print("Player 2 wins.")
+                        return True
+                        break
+            return False
+        
+        IsGameOver = boardcheck(self.boardmtx)
+        IsGameOver = IsGameOver or boardcheck(columns)
+        IsGameOver = IsGameOver or boardcheck(diagonals)
+        if IsGameOver is True:
+            return 1
+        else:
+            return 0
+        
+
 
 board = Board()
 player1 = HumanPlayer("Pefik",  1)
 player2 = HumanPlayer("Jarek", 2)
 
-player1.play(board)
-player2.play(board)
+while True:
+    player1.play(board)
+    gameOver = board.GameEnd()
+    if gameOver == 1:
+        break
+
+    player2.play(board)
+    gameOver = board.GameEnd()
+    if gameOver == 1:
+        break
+    
